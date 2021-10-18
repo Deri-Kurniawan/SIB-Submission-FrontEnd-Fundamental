daa1dfda517e3bf3f8eaca5d1e3e36c4b641eeec
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-18 11:46:08 +0700

    replace nav to app-bar tag element

--- src/scripts/components/app-bar.js
+++ src/scripts/components/app-bar.js
@@ -1,12 +1,14 @@
 class AppBar extends HTMLElement {
 
   connectedCallback() {
+    this.classList.add(
+      "navbar", "navbar-expand-lg", "navbar-light", "bg-light"
+    );
     this.render();
   }
 
   render() {
     this.innerHTML = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,7 +43,6 @@ class AppBar extends HTMLElement {
               </form>
             </div>
           </div>
-        </nav>
         `;
   }
 }
