e37c6ab5afed41881a48cbe7788870d0c20dcaee
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-18 10:41:39 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import $ from "jquery";
+import main from "./assets/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
