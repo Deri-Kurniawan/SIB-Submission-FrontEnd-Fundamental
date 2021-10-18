e04e5deede9b3cf9a51dd1e7b488c0a3428c00b0
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-18 23:24:19 +0700

    exports imports scripts have been moved

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
-import 'regenerator-runtime'; import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; import '../node_modules/bootstrap/dist/js/bootstrap.min'; import './scripts/components/app-bar'; import main from './scripts/view/main';
+import 'regenerator-runtime'; import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import main from './scripts/view/main';
 
 document.addEventListener('DOMContentLoaded', main);
--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import $ from 'jquery';
+import '../components/app-bar';
+import '../components/search-bar';
 
 const main = () => {
-
+  
 };
 
 export default main;
