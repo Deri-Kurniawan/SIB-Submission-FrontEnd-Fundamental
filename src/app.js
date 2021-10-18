7d4271379d6a66b168bf301576956582a29d34bf
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-18 11:39:30 +0700

    file moved

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import "regenerator-runtime";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./scripts/components/app-bar.js";
 import $ from "jquery";
-import main from "./assets/view/main.js";
+import main from "./scripts/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
