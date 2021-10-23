8f1132c4ad774f10ed68eb82a1dfa28687b20860
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-23 16:25:21 +0700

    change label

--- src/scripts/components/search-bar.js
+++ src/scripts/components/search-bar.js
@@ -8,8 +8,8 @@ class SearchBar extends HTMLElement {
   render() {
     this.innerHTML = `
       <form class="form-floating">
-        <input type="search" class="form-control" id="inputSearchElement" value="" placeholder="Search recipe"/>
-        <label class="inputSearchElement">Search recipe by food name</label>
+        <input type="search" class="form-control" id="inputSearchElement" title="Search by food name" placeholder="Search for food recipes by food name"/>
+        <label for="inputSearchElement" class="text-truncate col-12">Search by food name</label>
       </form>
     `;
   }
