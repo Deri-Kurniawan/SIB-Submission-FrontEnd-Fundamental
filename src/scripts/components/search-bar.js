35e1e4ddb5f7195f6f33a71f526845bfc5a09774
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-23 17:51:34 +0700

    remove title attribute

--- src/scripts/components/search-bar.js
+++ src/scripts/components/search-bar.js
@@ -8,7 +8,7 @@ class SearchBar extends HTMLElement {
   render() {
     this.innerHTML = `
       <form class="form-floating">
-        <input type="search" class="form-control" id="inputSearchElement" title="Search by food name" placeholder="Search for food recipes by food name"/>
+        <input type="search" class="form-control" id="inputSearchElement" placeholder="Search for food recipes by food name"/>
         <label for="inputSearchElement" class="text-truncate col-12">Search by food name</label>
       </form>
     `;
